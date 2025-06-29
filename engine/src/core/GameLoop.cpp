76f2bed33cba1a18f1ab470d31002a67457e01b8
Alessandro Mota <alessandro.mota@sittax.com.br>
2025-06-29 20:45:33 -0300

    (feat): gameloop extraction to separate file

---
+++ engine/src/core/GameLoop.cpp
@@ -0,0 +1,34 @@
+﻿#include "GameLoop.h"
+#include "../ecs/systems/player_control_system.h"
+#include <SDL3/SDL.h>
+
+#define FPS 60
+
+int run_game_loop(Application& app) {
+    const float dt = 1.0f / FPS;
+
+    SDL_Event e;
+
+    while (app.is_running()) {
+        Uint64 start = SDL_GetTicksNS();
+
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) {
+                app.stop();
+            }
+
+            player_control_system(app.get_registry(), e, app.get_player_speed());
+        }
+
+        app.update(dt);
+        app.render();
+
+        Uint64 elapsed = SDL_GetTicksNS() - start;
+        int delay_ms = int(1000 * dt) - (elapsed / 1000000ULL);
+        if (delay_ms > 0) {
+            SDL_Delay(delay_ms);
+        }
+    }
+
+    return 0;
+}
---
+++ engine/src/core/GameLoop.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+
+#include "Application.h"
+
+int run_game_loop(Application& app);
